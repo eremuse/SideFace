428a9e08b946d2906aab5d15e7aa205a9ab2698a
eremuse <99816636k@gmail.com>
2018-03-22 11:44:16 +0900

    1.Main-page 2.About-page 3.WorkFlow-page 4.Making-page 5.WorkStyle-page 6.Q&A-page 7.ContactUs-page 8.Galleries-page Total 8page (Web version) done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', {title:'Side Face'});
 });
 
+router.get('/galleries_color', function(req, res, next) {
+  res.render('galleries_color');
+});
+router.get('/galleries_emotion', function(req, res, next) {
+  res.render('galleries_emotion');
+});
+router.get('/galleries_extream', function(req, res, next) {
+  res.render('galleries_extream');
+});
+router.get('/galleries_movement', function(req, res, next) {
+  res.render('galleries_movement');
+});
+
+
 module.exports = router;
